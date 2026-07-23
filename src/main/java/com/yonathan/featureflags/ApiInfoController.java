4dcf04a9ef12bb23c4efb53fecbf76ef3192a5e4
Yonathan Abera <138686096+YonathanDAbera@users.noreply.github.com>
2026-07-22 23:03:51 -0400

    Restore API info controller

--- src/main/java/com/yonathan/featureflags/ApiInfoController.java
+++ src/main/java/com/yonathan/featureflags/ApiInfoController.java
@@ -3,8 +3,8 @@ package com.yonathan.featureflags;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
